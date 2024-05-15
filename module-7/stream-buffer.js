352ab555cc96ec3ff82ac1e738ed4d673a05a092
MD ABDULLAH <mdabdullah161036@gmail.com>
2024-05-15 12:40:19 +0600

    emmit with params

--- module-7/stream-buffer.js
+++ module-7/stream-buffer.js
@@ -6,5 +6,8 @@ const MyEventEmitter = new EventEmitter();
 MyEventEmitter.on('birthday', () => {
     console.log('happy birth day to you');
 });
+MyEventEmitter.on('birthday',(gift)=> {
+    console.log(`I will sent you a ${gift}`)
+})
 
-MyEventEmitter.emit('birthday')
+MyEventEmitter.emit('birthday','watch')
